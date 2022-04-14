897dec7347d1ed6e705839ef92d7e9ab71cd20e2
Patrick <petucker@columbus.gov>
2022-04-14 15:01:16 -0400

    strings commit

--- strings.py
+++ strings.py
@@ -1,1 +1,19 @@
 ## page 30
+
+poem = '''There was a Young Lady of Norway,
+Who casually sat in a doorway;
+When the door squeezed her flat,
+She exclaimed, "What of that?"
+This courageous Young Lady of Norway.'''
+print(poem)
+
+palindrome = 'A man,\nA plan,\nA canal:\nPanama.'
+print(palindrome)
+
+fact = "The world's largest rubber duck was 54'2\" by 65'7\" by 105'"
+print(fact)
+
+start = 'Na ' * 4 + '\n'
+middle = 'Hey ' * 3 + '\n'
+end = 'Goodbye.'
+print(start + start + middle + end)
