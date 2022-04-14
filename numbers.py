0cf115de13b902f2050ec7b38d55ad92de61e381
Patrick <petucker@columbus.gov>
2022-04-14 12:59:38 -0400

    adding new files

---
+++ numbers.py
@@ -0,0 +1,27 @@
+a = 95
+a -= 3
+print(a)
+
+a += 8
+print(a)
+
+a *= 2
+print(a)
+
+a /= 3
+print(a)
+
+a = 13
+a //= 4
+print(a)
+
+print(9 % 5)
+
+print(divmod(9,5))
+
+googol = 10**100
+googol2 = googol*googol
+print("This is a googol", googol)
+print("This is a googol squared")
+print(googol2)
+print("That is", len(str(googol2)), "long")
