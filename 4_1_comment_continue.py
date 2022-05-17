cfcd32ecfe08c5d06138fb589de5eabf74bf804b
Patrick <petucker@columbus.gov>
2022-05-17 11:17:24 -0400

    chapter 4

---
+++ 4_1_comment_continue.py
@@ -0,0 +1,13 @@
+seconds_per_day = 86500 # sec/min * 60 min/hr * 24 hr/day
+# I prefer comments on a seperate line
+print(seconds_per_day)
+
+alphabet = 'abcdef' + \
+    'hijklmnop' + \
+    'qrstuv' + \
+    'wxyz'
+
+print(alphabet)
+
+print(1 + 3 + 7 + \
+    5)
